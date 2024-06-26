0375f7a0714b3eb6005dc7da4960583138989801
SempoCode <sempo.code@gmail.com>
2024-06-26 02:59:24 -0700

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,9 @@
--- List all shows without the genre Comedy in hbtn_0d_tvshows
-SELECT title 
+-- List all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- Subquery to get the Comedy genre id
+-- Main query to list shows without the Comedy genre
+SELECT tv_shows.title
 FROM tv_shows 
-WHERE id NOT IN (
-    SELECT show_id 
-    FROM tv_show_genres 
-    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
-) 
-ORDER BY title ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+AND tv_show_genres.genre_id = (SELECT @comedy_id := id FROM tv_genres WHERE name = 'Comedy')
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC;
